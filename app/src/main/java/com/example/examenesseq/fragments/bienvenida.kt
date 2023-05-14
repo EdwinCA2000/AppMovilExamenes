401a3b53c1c57ff08a6919c27a93ee57ac38e1e9
Edwin Canul <comerseanhg@hotmail.com>
2023-05-14 16:23:45 -0500

    Agregado la interfaz de la bienvenida y el login

--- app/src/main/java/com/example/examenesseq/fragments/bienvenida.kt
+++ app/src/main/java/com/example/examenesseq/fragments/bienvenida.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.examenesseq.R
+import com.example.examenesseq.databinding.FragmentBienvenidaBinding
 
 class bienvenida : Fragment() {
+
+    private var _binding: FragmentBienvenidaBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bienvenida, container, false)
+        _binding = FragmentBienvenidaBinding.inflate(inflater, container, false)
+        actions()
+        return binding.root
+    }
+
+    private fun actions() {
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_bienvenida_to_login)
+        }
+
+        binding.btnRegistro.setOnClickListener {
+            findNavController().navigate(R.id.action_bienvenida_to_registro)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
