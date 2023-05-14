c2fc746ebdd6faf2a614d9b418ecf66fc99718f4
Edwin Canul <comerseanhg@hotmail.com>
2023-05-14 17:22:57 -0500

    Se agrego el binding al login.kt

--- app/src/main/java/com/example/examenesseq/fragments/login.kt
+++ app/src/main/java/com/example/examenesseq/fragments/login.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.examenesseq.R
+import com.example.examenesseq.databinding.FragmentLoginBinding
 
 class login : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.btnIniciarSesion.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_inicio)
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
