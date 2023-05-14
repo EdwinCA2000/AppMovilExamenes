305de9e67f2c3961a1a017057b364b6a35a99a56
Edwin Canul <comerseanhg@hotmail.com>
2023-05-14 14:23:32 -0500

    Se movio los fragments a otra carpeta y se ajusto el navhostFragment a la pantalla del móvil

--- app/src/main/java/com/example/examenesseq/fragments/bienvenida.kt
+++ app/src/main/java/com/example/examenesseq/fragments/bienvenida.kt
@@ -1,19 +1,20 @@
-package com.example.examenesseq
+package com.example.examenesseq.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.examenesseq.R
 
-class inicio : Fragment() {
+class bienvenida : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        return inflater.inflate(R.layout.fragment_bienvenida, container, false)
     }
 
 }
--- app/src/main/java/com/example/examenesseq/fragments/login.kt
+++ app/src/main/java/com/example/examenesseq/fragments/login.kt
@@ -1,10 +1,11 @@
-package com.example.examenesseq
+package com.example.examenesseq.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.examenesseq.R
 
 class login : Fragment() {
 
