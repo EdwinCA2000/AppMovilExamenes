e50a69f4d83eeb28e279359b72b3e8a740d7a303
Edwin Canul <comerseanhg@hotmail.com>
2023-05-15 22:02:11 -0500

    Se agrego la api del proyecto para iniciar sesión

--- app/src/main/java/com/example/examenesseq/datos/ApiServicio.kt
+++ app/src/main/java/com/example/examenesseq/datos/ApiServicio.kt
@@ -4,17 +4,16 @@ import com.example.examenesseq.datos.respuesta.LoginRespuesta
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiServicio {
-    @POST(value="login")
-    fun postLogin(@Query(value="email")email: String, @Query(value="password") password: String):
+    @POST(value="tecnologiaeducativa.Sesion/actionLogin")
+    fun postLogin(@Query(value="Usuario")usuario: String, @Query(value="Password") password: String):
             Call<LoginRespuesta>
 
     companion object Factory{
-        private const val BASE_URL="http://192.168.0.11:8080/api"
+        private const val BASE_URL="http://192.168.0.3:8080/Ceneval/"
         fun create(): ApiServicio{
 
             val retrofit= Retrofit.Builder()
