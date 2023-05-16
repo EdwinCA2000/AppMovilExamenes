fa431bef3a5c85ba0919408aabc0110a4691e446
Edwin Canul <comerseanhg@hotmail.com>
2023-05-15 22:02:54 -0500

    Se módifico la respuesta de la petición

--- app/src/main/java/com/example/examenesseq/datos/respuesta/LoginRespuesta.kt
+++ app/src/main/java/com/example/examenesseq/datos/respuesta/LoginRespuesta.kt
@@ -1,9 +1,6 @@
 package com.example.examenesseq.datos.respuesta
 
-import com.example.examenesseq.model.Usuario
-
 data class LoginRespuesta(
-    val success: Boolean,
-    val usuario: Usuario,
-    val jwt: String
+    val Error: Int,
+    val Mensaje: String
 )
